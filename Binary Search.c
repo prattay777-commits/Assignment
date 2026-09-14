ad22906020a31d8703feca6549865ac3be7273f6
prattay777-commits <prattaygupta0@gmail.com>
2026-09-15 02:22:40 +0600

    Add files via upload

---
+++ Binary Search.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, key;
+    int low, high, mid, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted elements: ");
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (a[mid] == key)
+        {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+        else if (a[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
